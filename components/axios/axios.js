81d20a7fa81dd4f1b9c28cb5b55df8d7a4c73616
Banee Ishaque K <Baneeishaque@gmail.com>
2024-09-20 21:53:43 +0530

    feat: hosted backend instead of local backend

--- components/axios/axios.js
+++ components/axios/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: 'http://localhost:5000/'
-    // baseURL: 'http://13.233.174.41:5000/'
-
+    // baseURL: 'http://localhost:5000/'
+    baseURL: 'http://13.201.152.51:5000/'
 })
 
 export default Axios
